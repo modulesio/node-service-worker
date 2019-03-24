5e3dad3a39f6fd3751f505d65176ca970fbb31a2
Avaer Kazmer <a@modules.io>
2019-03-24 00:41:39 +0000

    Initial rewrite into window-sw

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const ServiceWorkerGlobalScope = require('./worker/serviceworker-global-scope');
-// const getAllKeys = require('./util/get-all-keys');
+const path = require('path');
+
+const fetch = require('window-fetch');
+const nativeWorker = require('worker-native');
+
+/* const ServiceWorkerGlobalScope = require('./worker/serviceworker-global-scope');
 const FetchEvent = require('./worker/fetchevent');
 const url = require('url');
 const ExtendableEvent = require('./worker/extendable-event');
@@ -7,46 +11,65 @@ const fetch = require('node-fetch');
 const applyFuncWithGlobals = require('./util/apply-func-with-globals');
 
 const installAndActivate = require('./util/install-and-activate');
-const resolveExtendableEvent = require('./util/resolve-extendable-event');
+const resolveExtendableEvent = require('./util/resolve-extendable-event'); */
 
 class ServiceWorker {
-    
-    constructor({scriptURL, scope, contents, interceptFetch, importScript}) {
-        this.globalScope = new ServiceWorkerGlobalScope({scope, interceptFetch, importScript});
-        this.caches = this.globalScope.caches;
-        this.globalScope.registration.scope = scope;
-        this.dispatchEvent = this.globalScope.dispatchEvent.bind(this.globalScope)
+  // constructor({scriptURL, scope, contents, interceptFetch, importScript}) {
+  constructor(scriptUrl, scope) {
+    const worker = nativeWorker.make({
+      initModule: path.join(__dirname, 'src', 'service-worker.js'),
+      args: {
+        scriptUrl,
+        scope,
+      },
+    });
+    /* worker.on('error', err => {
+      console.warn(err.stack);
+    }); */
 
-        applyFuncWithGlobals(this.globalScope, contents);
+    /* this.globalScope = new ServiceWorkerGlobalScope({scope, interceptFetch, importScript});
+    this.caches = this.globalScope.caches;
+    this.globalScope.registration.scope = scope;
+    this.dispatchEvent = this.globalScope.dispatchEvent.bind(this.globalScope)
 
-        // We need to grab items from our globalScope object and apply them to the 
-        // actual global scope the worker will run in.
-        // let keysToApplyToGlobal = getAllKeys(this.globalScope);
-        // let objectsToApplyToGlobal = keysToApplyToGlobal.map((key) => {
+    applyFuncWithGlobals(this.globalScope, contents); */
 
-        //     if (typeof this.globalScope[key] === "function") {
-        //         // If it's a function we want to ensure that it stays bound
-        //         // to the global scope object.
-        //         return this.globalScope[key].bind(this.globalScope);
-        //     } else {
-        //         return this.globalScope[key];
-        //     }
-            
-        // });
+    // We need to grab items from our globalScope object and apply them to the 
+    // actual global scope the worker will run in.
+    // let keysToApplyToGlobal = getAllKeys(this.globalScope);
+    // let objectsToApplyToGlobal = keysToApplyToGlobal.map((key) => {
 
-        // keysToApplyToGlobal.forEach((key, i) => {
-        //     this[key] = objectsToApplyToGlobal[i];
-        // });
-    }
+    //     if (typeof this.globalScope[key] === "function") {
+    //         // If it's a function we want to ensure that it stays bound
+    //         // to the global scope object.
+    //         return this.globalScope[key].bind(this.globalScope);
+    //     } else {
+    //         return this.globalScope[key];
+    //     }
+        
+    // });
 
+    // keysToApplyToGlobal.forEach((key, i) => {
+    //     this[key] = objectsToApplyToGlobal[i];
+    // });
+  }
 }
 
 module.exports = {
-    ServiceWorker,
-    FetchEvent,
-    ExtendableEvent,
-    Request: fetch.Request,
-    Response: fetch.Response,
-    installAndActivate,
-    resolveExtendableEvent
+  register(scriptUrl, options = {}) {
+    if (!options.scope) {
+      options.scope = 'file://' + __dirname + '/';
+    }
+    const serviceWorker = new ServiceWorker(scriptUrl, options.scope);
+    return Promise.resolve({
+      // XXX ServiceWorkerRegistration
+    });
+  },
+  /* ServiceWorker,
+  FetchEvent,
+  ExtendableEvent,
+  Request: fetch.Request,
+  Response: fetch.Response,
+  installAndActivate,
+  resolveExtendableEvent */
 }
